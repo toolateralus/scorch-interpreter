bf5b8d4d0d637d3632505ae5e363694be4051189
toolateralus <joshnjoshgaming@gmail.com>
2023-12-28 13:12:35 -0500

    update sterr_dump script & removed an old setting

--- scripts/sterr_dump.sh
+++ scripts/sterr_dump.sh
@@ -4,13 +4,4 @@ mkdir -p logs/
 # 2>&1 means print both stderr and std out. redirect to file
 cargo run -- dump >logs/stderr_dump.txt 2>&1 
 
-# Get the absolute path of the script file
-SCRIPT_PATH=$(readlink -f "$0")
-
-# Extract the test name from the script path
-TEST_NAME=$(basename "$SCRIPT_PATH")
-
-# Print the final full path of the new output file
-echo "Output file path: $SCRIPT_PATH/logs/stderr_dump.txt"
-echo "Test name: $TEST_NAME"
-
+echo "Output file path: logs/stderr_dump.txt"
