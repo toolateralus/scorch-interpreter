b652c05682ce700ffe94b764a64422cd8dda753a
Your Name <joshnjoshgaming@gmail.com>
2023-12-30 17:43:32 -0500

    added run_with_modules fn

--- src/lib.rs
+++ src/lib.rs
@@ -38,3 +38,36 @@ pub fn run<'a>(code : &'a String) -> Result<&'a Value, String> {
     
     Ok(&Value::None())   
 }
+
+pub fn run_with_modules<'a>(code_array : Vec<String>) -> Result<Value, String> {
+    
+    let mut interpreter = Interpreter::new();
+    
+    let mut result : Option<Value> = None;    
+    
+    for code in code_array {
+        let mut lexer = lexer::create_tokenizer();
+    
+        lexer.tokenize(&code);
+        
+        let tokens = &lexer.tokens;
+        
+        let ast_root = parser::parse_program(&tokens);
+            
+            let Ok(ast_root) = ast_root else {
+                let Err(err) = ast_root else {
+                    panic!("Failed to parse input:");
+                };
+                
+                dbg!(err);
+                panic!();
+            };
+            
+        result = Some(ast_root.accept(&mut interpreter));
+    }
+    
+    match result.clone() {
+        Some(value) => return Ok(value),
+        None => return Err("No result".to_string())
+    }
+}
